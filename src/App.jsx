4e7142077245f91a49d4906714f3a5e65233dc94
avishya123 <avishyaachu@gmail.com>
2024-06-11 05:40:31 -0700

    dollar_exchange

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUp";
 import ForgotPasswordPage from "./pages/ForgotPassword";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./pages/Login";
+import '@fortawesome/fontawesome-free/css/all.css';
 function App() {
   return (
     <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
     
     </Routes>
