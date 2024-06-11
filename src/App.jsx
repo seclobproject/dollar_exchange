b4aa884b10f945c4eed8ce10adbdaf8a14d6fed3
Jithin C <jith97972@gmail.com>
2024-06-11 13:16:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+function App() {
+  return (
+    <div className='bg-custom-blue'>
+    <Header/>
+    <Hero/>
+    </div>
+  )
+}
+
+export default App
